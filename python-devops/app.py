aa7ca98ba8d7bb7ba0c525516881bcba8e30250c
Bruno Mendes <bmendesl@4linux.com>
2018-12-14 22:06:02 -0200

    Docker

--- python-devops/app.py
+++ python-devops/app.py
@@ -1,14 +1,15 @@
 #!/home/developer/521Noturno/python-devops/venvdevops/bin/python3
 
 from flask import Flask, render_template
-from docker.bdocker import docker
+from pdocker.bdocker import docker
 
 app  = Flask(__name__)
-app.register_blueprint(docker)
+app.register_blueprint(docker) #registro a minha rota com blueprint
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5050)
