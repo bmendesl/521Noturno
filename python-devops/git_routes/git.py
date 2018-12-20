91a355bcefa1917ef9582ac81820661398178090
Bruno Mendes <bmendesl@4linux.com>
2018-12-19 22:29:37 -0200

    gitlab2

--- python-devops/git_routes/git.py
+++ python-devops/git_routes/git.py
@@ -1,8 +1,14 @@
 from flask import Flask, Blueprint, render_template
-
+from gitlab import Gitlab
 
 gitlab = Blueprint('gittlab', __name__, url_prefix='/gitlab')
 
+con = Gitlab('http://172.17.0.3', private_token='x9wyRGV97wZa1NeG23DR')
+
 @gitlab.route('')
 def index():
-    return render_template('gitlab.html')
+    users = con.users.list()
+    projects = con.projects.list()
+    return render_template('gitlab.html', users=users, projects=projects)
+
+
